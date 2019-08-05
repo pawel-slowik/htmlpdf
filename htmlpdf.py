a08a66cf891da4045e9dcc909f6a7e6d8a7540d2
Paweł Słowik <pawel.slowik@zoho.com>
2019-08-05 21:39:06 +0200

    Add docstrings

--- htmlpdf.py
+++ htmlpdf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""A tool for creating PDF documents from YAML data and Jinja2 HTML templates."""
 
 import re
 import collections.abc
@@ -17,11 +18,17 @@ Leaf = Union[str, numbers.Real, None]
 Node = Union[Mapping, Iterable, Leaf]
 
 def create_pdf(html: str, base_url: str, output_filename: str) -> None:
+    """Create a PDF file from an HTML document."""
     font_config = weasyprint.fonts.FontConfiguration()
     document = weasyprint.HTML(string=html, base_url=base_url)
     document.write_pdf(target=output_filename, font_config=font_config)
 
 def render_html(data_filenames: Iterable[str], html_template_filename: str) -> str:
+    """Render a Jinja2 template using YAML files as data sources.
+
+    Data from any subsequent YAML file overwrites data loaded from previous
+    file(s). You can refer to nodes from previous files using YAML anchors and
+    aliases: https://yaml.org/spec/1.2/spec.html#alias// """
     data = recursive_map(
         yaml.safe_load("\n".join(open(df, "r").read() for df in data_filenames)),
         process_tags
@@ -30,6 +37,7 @@ def render_html(data_filenames: Iterable[str], html_template_filename: str) -> s
     return jinja2.Template(html_template).render(data)
 
 def process_tags(inp: Leaf) -> Leaf:
+    """Process a limited subset of Markdown into HTML."""
     if isinstance(inp, numbers.Real):
         return inp
     if inp is None:
@@ -43,6 +51,7 @@ def process_tags(inp: Leaf) -> Leaf:
     return inp
 
 def recursive_map(node: Node, func: Callable[[Leaf], Leaf]) -> Node:
+    """Apply a function recursively to a node (similar to map(), but recursive)."""
     if isinstance(node, str):
         return func(node)
     if isinstance(node, numbers.Real):
@@ -56,24 +65,34 @@ def recursive_map(node: Node, func: Callable[[Leaf], Leaf]) -> Node:
     raise ValueError
 
 def get_title(html: str) -> str:
+    """Extract the meta title from an HTML document."""
     wrapper = ElementWrapper.from_html_root(ElementTree.fromstring(html))
     meta: Mapping[str, str] = weasyprint.html.get_html_metadata(wrapper, "")
     return meta["title"]
 
 def create_output_filename(title: str) -> str:
+    """Create a CLI friendly filename (no spaces, no accented characters etc.)"""
     ascii_title: str = unidecode(title)
     return re.sub(r"\W+", "_", ascii_title) + ".pdf"
 
 def get_base_url(filename: str) -> str:
+    """Get a base URL that is usable as WeasyPrint base_url parameter."""
     dirname = os.path.dirname(filename)
     return dirname if dirname else "."
 
 def enable_weasyprint_logging() -> None:
+    """Make the WeasyPrint warnings visible.
+
+    Most HTML / CSS errors are not fatal and will not prevent WeasyPrint from
+    rendering a document, but they can make the document appear ugly or broken.
+    Therefore it's important to take heed of the warning messages.
+    https://weasyprint.readthedocs.io/en/latest/tutorial.html#logging"""
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter("WeasyPrint %(levelname)s: %(message)s"))
     weasyprint.LOGGER.addHandler(handler)
 
 def main() -> None:
+    """A simple CLI for the module. Run with `-h` for help."""
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument(
